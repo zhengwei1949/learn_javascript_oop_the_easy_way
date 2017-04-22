e1ce05cccc2263f64977ba9591a8b58ca76e84df
zhengwei1949 <zhengwei1949@qq.com>
2017-04-22 15:29:52 +0800

    a

---
+++ 0.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        //需求：用js来表示一只猫
+        var name = '猫';
+        var age = 2;
+
+        //缺点 1. 不能直观的看出来name,age是表示的同一组信息
+    </script>
+</body>
+</html>
