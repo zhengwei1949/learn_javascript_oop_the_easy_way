26f2a59b9256e796a68e92ed3fa26c85c9f323cb
zhengwei1949 <zhengwei1949@qq.com>
2018-01-05 11:22:30 +0800

    Update 9.js

--- 2.对象、构造函数、继承与es6中类及继承写法/9.js
+++ 2.对象、构造函数、继承与es6中类及继承写法/9.js
@@ -8,10 +8,12 @@ Cat.prototype.say = function(){
 };
 
 
-function HelloKitty(name,age){
-    Cat.call(this,name,age);
-    this.style = 'very cute';
-}
+function HelloKitty(name, age) {
+            // Cat.call(this, name, age);这样写可以一行搞定，但不太好理解，可以换成如下的二行代码
+            this.Cat = Cat;
+            this.Cat(name,age);
+            this.style = 'very cute';
+        }
 
 HelloKitty.prototype = new Cat();
 
